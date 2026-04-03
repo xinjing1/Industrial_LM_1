a3e677caf65ab17339d8518eda1d4bcca541395d
xinjing1 <2609697929@qq.com>
2026-04-03 18:04:03 +0800

    调整 Vercel 部署路径配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: '/Industrial_LM_1/',
-})
+  base: process.env.VERCEL ? '/' : '/Industrial_LM_1/',
+}))
